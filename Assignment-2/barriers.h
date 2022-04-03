b330f967f90b50f091593b505043f39916ecefe0
Tushar Singla <singlatushar07@gmail.com>
2022-04-03 19:31:55 +0530

    Fixed CV tree barrier

--- Assignment-2/barriers.h
+++ Assignment-2/barriers.h
@@ -14,10 +14,11 @@ typedef struct Tree_Sense_Reversing_t {
     int **flag;
 } Tree_Sense_Reversing_t;
 
-// typedef struct Tree_Posix_CV_t {
-//     pthread_cond_t **cv;
-//     pthread_mutex_t *mutex;
-// } Tree_Posix_CV_t;
+typedef struct Tree_Posix_CV_t {
+    pthread_cond_t **cv;
+    pthread_mutex_t **mutex;
+    int **flag;
+} Tree_Posix_CV_t;
 
 void Central_Sense_Reversing_Init(Central_Sense_Reversing_t *barrier);
 void Central_Sense_Reversing_Wait(Central_Sense_Reversing_t *barrier, int *localsense, int num_threads);
@@ -25,5 +26,5 @@ void Central_Posix_CV_Init(Central_Posix_CV_t *barrier);
 void Central_Posix_CV_Wait(Central_Posix_CV_t *barrier, int num_threads);
 void Tree_Sense_Reversing_Init(Tree_Sense_Reversing_t *barrier, int num_threads);
 void Tree_Sense_Reversing_Wait(Tree_Sense_Reversing_t *barrier, int thread_id, int num_threads);
-// void Tree_Posix_CV_Init(Tree_Posix_CV_t *barrier, int num_threads);
-// void Tree_Posix_CV_Wait(Tree_Posix_CV_t *barrier, int thread_id, int num_threads);
+void Tree_Posix_CV_Init(Tree_Posix_CV_t *barrier, int num_threads);
+void Tree_Posix_CV_Wait(Tree_Posix_CV_t *barrier, int thread_id, int num_threads);
